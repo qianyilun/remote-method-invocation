d6aef97e4cf7f89767e3d1f939df5c2eb0ed5223
MATH232 <yy9577@gmail.com>
2019-01-16 13:42:05 -0800

    Improved the error handling when server is down or connecton timeout

--- Client.java
+++ Client.java
@@ -5,7 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Client {
-    private static final String IP_ADDRESS = "142.58.15.52";
+    private static final String IP_ADDRESS = "206.12.16.2";
     private static final int PORT = 5656;
 
     private Client() {
@@ -19,48 +19,47 @@ public class Client {
         long startTimeInMillis = System.currentTimeMillis();
 
         // get the server time in milliseconds
-        long serverTimeInMillis = client.getServerTimeInMilliseconds();
-
-        // get the client time after calling the server
-        long endTimeInMillis = System.currentTimeMillis();
-
-        // calculate the new client time using client time + RTT / 2
-        long calculatedClientTime = (endTimeInMillis - startTimeInMillis) / 2 + serverTimeInMillis;
-
-        // create a new date object based on the calculated time for client
-        Calendar newClientTime = Calendar.getInstance();
-        newClientTime.setTimeInMillis(calculatedClientTime);
-
-        System.out.println("calculated Client Time in Milliseconds is: " + calculatedClientTime);
-        System.out.println("calculated Client Time is: " + newClientTime.getTime());
-
-        // set windows system time using our new date object
-        client.setWindowsTime(newClientTime.getTime());
-    }
-
-    private long getServerTimeInMilliseconds() {
-        long timeInMillis = 0;
         try {
-            // Getting the registry
-            System.out.println("Connection to server with IP address: " + IP_ADDRESS + ", port: " + PORT);
-            Registry registry = LocateRegistry.getRegistry(IP_ADDRESS, PORT);
+            long serverTimeInMillis = client.getServerTimeInMilliseconds();
+            // get the client time after calling the server
+            long endTimeInMillis = System.currentTimeMillis();
 
-            // Looking up the registry for the remote object
-            ImplInterface stub = (ImplInterface) registry.lookup("Impl");
+            // calculate the new client time using client time + RTT / 2
+            long calculatedClientTime = (endTimeInMillis - startTimeInMillis) / 2 + serverTimeInMillis;
 
-            // Calling the remote method using the obtained object, getServerTime will
-            // return the server time as a date object
-            Date serverDate = stub.getServerTime();
+            // create a new date object based on the calculated time for client
+            Calendar newClientTime = Calendar.getInstance();
+            newClientTime.setTimeInMillis(calculatedClientTime);
 
-            // get the server time in milliseconds to make the calculation easier
-            timeInMillis = serverDate.getTime();
+            System.out.println("calculated Client Time in Milliseconds is: " + calculatedClientTime);
+            System.out.println("calculated Client Time is: " + newClientTime.getTime());
 
-            System.out.println("Server time is: " + serverDate);
-            System.out.println("Server time in milliseconds is: " + timeInMillis);
-        } catch (Exception e) {
-            System.err.println("Client exception: " + e.toString());
+            // set windows system time using our new date object
+            client.setWindowsTime(newClientTime.getTime());
+        } catch (Exception e) {            
+            System.out.println("Failed to connect to the server");            
             e.printStackTrace();
         }
+    }
+
+    private long getServerTimeInMilliseconds() throws Exception {
+        long timeInMillis = 0;        
+        // Getting the registry
+        System.out.println("Connecting to server with IP address: " + IP_ADDRESS + ", port: " + PORT);
+        Registry registry = LocateRegistry.getRegistry(IP_ADDRESS, PORT);        
+
+        // Looking up the registry for the remote object
+        ImplInterface stub = (ImplInterface) registry.lookup("Impl");
+
+        // Calling the remote method using the obtained object, getServerTime will
+        // return the server time as a date object
+        Date serverDate = stub.getServerTime();
+
+        // get the server time in milliseconds to make the calculation easier
+        timeInMillis = serverDate.getTime();
+
+        System.out.println("Server time is: " + serverDate);
+        System.out.println("Server time in milliseconds is: " + timeInMillis);
 
         return timeInMillis;
     }
